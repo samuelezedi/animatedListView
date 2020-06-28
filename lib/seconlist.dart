097543535477f7c09f2ca807269a6f8acb897e21
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-28 17:01:30 +0100

    started animatedlist afresh

--- lib/seconlist.dart
+++ lib/seconlist.dart
@@ -7,77 +7,32 @@ class VerticalList extends StatefulWidget {
   _VerticalListState createState() => _VerticalListState();
 }
 
-class _VerticalListState extends State<VerticalList> with SingleTickerProviderStateMixin  {
+class _VerticalListState extends State<VerticalList> {
   var listData = youtubeData;
-  Animation animation;
-  AnimationController animationController;
+  GlobalKey<AnimatedListState> key = GlobalKey();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController = AnimationController(duration: Duration(seconds: 2,), vsync: this);
-    animationController = Tween(begin: 0.0, end: 1.0)
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
         title: Text('Vertical List'),
         elevation: 0,
       ),
-      body: NotificationListener(
-        onNotification: (notification){
+      body: AnimatedList(
+        key: key,
+        initialItemCount: listData.length,
+        itemBuilder: (context, index, animation) {
 
         },
-        child: ListView.builder(
-          itemCount: listData.length,
-            itemBuilder: (context, index) {
-              return AnimatedContainer(
-                duration: Duration(milliseconds: 500),
-                curve: Curves.fastOutSlowIn,
-                width: double.infinity,
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(11.0),
-                  color: Colors.white
-                ),
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(11.0),
-                          image: DecorationImage(
-                              image: NetworkImage(listData[index].thumbNail),
-                              fit: BoxFit.cover
-                          )
-                      ),
-
-                    ),
-                    Text(
-                      listData[index].channelTitle,
-                      style: TextStyle(
-                        fontFamily: 'Rubik',
-                        fontSize: 16,
-                        color: const Color(0xff5927ff),
-                        fontWeight: FontWeight.w500,
-                        height: 2,
-                      ),
-                      textAlign: TextAlign.left,
-                    ),
-                  ],
-                ),
-              );
-            }
-        ),
-      ),
+      )
     );
   }
 }
