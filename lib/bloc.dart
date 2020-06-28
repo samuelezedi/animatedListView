dfe9e6d925a82232ea23d221c82f456f9753fbd6
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-28 15:26:41 +0100

    added, provider

---
+++ lib/bloc.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+
+class Bloc extends ChangeNotifier {
+
+  List<double> _width;
+  List<double> _heights;
+
+  getWidth(int index) {
+    return _width[index];
+  }
+
+  getHeigth(int index) {
+    return _heights[index];
+  }
+
+  setWidth(double value){
+    _width.add(value);
+  }
+
+  setHeight(double value){
+    _heights.add(value);
+  }
+
+  changeWidth(int index, double value) {
+    _width[index]=value;
+    notifyListeners();
+  }
+
+  changeHeight(int index, double value) {
+    _heights[index] = value;
+    notifyListeners();
+  }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:animatedlistview/bloc.dart';
 import 'package:animatedlistview/models/list_data.dart';
 import 'package:flutter/material.dart';
 import 'package:visibility_detector/visibility_detector.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,68 +25,104 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class AnimatedListView extends StatefulWidget {
+class AnimatedListView extends StatefulWidget  {
   @override
   _AnimatedListViewState createState() => _AnimatedListViewState();
 }
 
-class _AnimatedListViewState extends State<AnimatedListView> {
+class _AnimatedListViewState extends State<AnimatedListView> with SingleTickerProviderStateMixin  {
 
+  Animation animation;
+  AnimationController animationController;
   var listData = youtubeData;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Animated ListView'),
         elevation: 0,
       ),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: (notification){
+      body: ChangeNotifierProvider(
+        create: Bloc(),
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (notification){
 
-        },
-        child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index){
-                return VisibilityDetector(
-                  key: Key("unique-key-${index.toString()}"),
-                  onVisibilityChanged: (VisibilityInfo info) {
-                    debugPrint("${info.visibleFraction} of my widget is visible");
-                  },
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        width: 120.0,
-                        height: 120.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(18.0),
-                          image: DecorationImage(
-                            image: NetworkImage(listData[index].thumbNail)
-                          )
-                        ),
+          },
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.bounceOut,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index){
+                  var sp = Provider.of<Bloc>(context);
+                  sp.setWidth(index, 100);
+                  sp.setHeight(index, 100);
+                    return VisibilityDetector(
+                      key: Key("unique-key-${index.toString()}"),
+                      onVisibilityChanged: (VisibilityInfo info) {
 
-                      ),
-                      Text(
-                        listData[index].channelTitle,
-                        style: TextStyle(
-                          fontFamily: 'Rubik',
-                          fontSize: 16,
-                          color: const Color(0xff5927ff),
-                          fontWeight: FontWeight.w500,
-                          height: 2,
+                        if(info.visibleFraction == 1.0){
+                          width = 150;
+                          height = 150;
+                          print(true);
+                        } else {
+                          width = 100;
+                          height = 100;
+                          print(false);
+                        }
+                        print(width);
+                        debugPrint("${info.visibleFraction} of my widget is visible ${index.toString()}");
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width: width,
+                              height: height,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(11.0),
+                                image: DecorationImage(
+                                  image: NetworkImage(listData[index].thumbNail),
+                                  fit: BoxFit.cover
+                                )
+                              ),
+
+                            ),
+                            Text(
+                              listData[index].channelTitle,
+                              style: TextStyle(
+                                fontFamily: 'Rubik',
+                                fontSize: 16,
+                                color: const Color(0xff5927ff),
+                                fontWeight: FontWeight.w500,
+                                height: 2,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
                         ),
-                        textAlign: TextAlign.left,
                       ),
-                    ],
-                  ),
-                );
-            },
-            separatorBuilder: (context, index){
-              return SizedBox(
-                width: 15,
-              );
-            },
-            itemCount: listData.length),
+                    );
+                },
+                separatorBuilder: (context, index){
+                  return SizedBox(
+                    width: 15,
+                  );
+                },
+                itemCount: listData.length),
+          ),
+        ),
       ),
     );
   }
